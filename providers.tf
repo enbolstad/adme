1d0032638bfa962a5f2da7b8c1014670f2cdf98e
Eirik <eirik.bols@outlook.com>
2025-03-17 12:04:15 +0100

    added backend-config in providers.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,11 @@ terraform {
       version = "4.22.0"
     }
   }
+  backend "azurerm" {
+    key                  = "github.terraform.tfstate"
+  }
+
+  required_version = ">=0.12"
 }
 provider "azurerm" {
   features {}
